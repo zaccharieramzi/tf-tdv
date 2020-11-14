6814f77db33f9da6cca55d6bd40b7fecde1bfeaa
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-14 17:25:43 +0100

    added more tests for tdv archi

--- tdv_test.py
+++ tdv_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 import tensorflow as tf
 
 from tdv import TDV
@@ -14,10 +15,12 @@ def test_tdv_energy():
     res = model.energy(tf.zeros(shape))
     assert res.shape.as_list() == [1, 1]
 
-def test_tdv_call():
+@pytest.mark.parametrize('n_macro', [2, 3])
+@pytest.mark.parametrize('n_scales', [2, 3])
+def test_tdv_call(n_macro, n_scales):
     model = TDV(
-        n_macro=2,
-        n_scales=2,
+        n_macro=n_macro,
+        n_scales=n_scales,
         n_filters=8,
     )
     shape = [1, 32, 32, 1]
