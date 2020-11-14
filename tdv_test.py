d2306dbcc9585f7ec2ac156f19bbd6b65045d2ec
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-14 17:29:40 +0100

    added back the model var change in tdv test

--- tdv_test.py
+++ tdv_test.py
@@ -28,18 +28,18 @@ def test_tdv_call(n_macro, n_scales):
     assert res.shape.as_list() == shape
 
 
-# def test_model_change():
-#     model = TDV(
-#         n_macro=2,
-#         n_scales=2,
-#         n_filters=8,
-#     )
-#     x = tf.random.normal((1, 64, 64, 1))
-#     y = x
-#     model(x)
-#     before = [v.numpy() for v in model.trainable_variables]
-#     model.compile(optimizer='sgd', loss='mse')
-#     model.train_on_batch(x, y)
-#     after = [v.numpy() for v in model.trainable_variables]
-#     for b, a in zip(before, after):
-#         assert np.any(np.not_equal(b, a))
+def test_tdv_change():
+    model = TDV(
+        n_macro=2,
+        n_scales=2,
+        n_filters=8,
+    )
+    x = tf.random.normal((1, 32, 32, 1))
+    y = tf.random.normal((1, 32, 32, 1))
+    model(x)
+    before = [v.numpy() for v in model.trainable_variables]
+    model.compile(optimizer='sgd', loss='mse')
+    model.train_on_batch(x, y)
+    after = [v.numpy() for v in model.trainable_variables]
+    for b, a in zip(before, after):
+        assert np.any(np.not_equal(b, a))
