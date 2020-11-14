a17a3c3e6f3531449ffc8399eff12c7a9daa0405
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-14 16:29:43 +0100

    added tdv test for energy

--- tdv_test.py
+++ tdv_test.py
@@ -1,28 +1,32 @@
 import numpy as np
 import tensorflow as tf
 
-from model import Model
+from tdv import TDV
 
 
-def test_model():
-    model = Model(
-        layers_n_channels=[4, 8],
+def test_tdv_energy():
+    model = TDV(
+        n_macro=2,
+        n_scales=2,
+        n_filters=8,
     )
     shape = [1, 32, 32, 1]
-    res = model(tf.zeros(shape))
-    assert res.shape.as_list() == shape
+    res = model.energy(tf.zeros(shape))
+    assert res.shape.as_list() == (1, 1)
 
 
-def test_model_change():
-    model = Model(
-        layers_n_channels=[4, 8],
-    )
-    x = tf.random.normal((1, 64, 64, 1))
-    y = x
-    model(x)
-    before = [v.numpy() for v in model.trainable_variables]
-    model.compile(optimizer='sgd', loss='mse')
-    model.train_on_batch(x, y)
-    after = [v.numpy() for v in model.trainable_variables]
-    for b, a in zip(before, after):
-        assert np.any(np.not_equal(b, a))
+# def test_model_change():
+#     model = TDV(
+#         n_macro=2,
+#         n_scales=2,
+#         n_filters=8,
+#     )
+#     x = tf.random.normal((1, 64, 64, 1))
+#     y = x
+#     model(x)
+#     before = [v.numpy() for v in model.trainable_variables]
+#     model.compile(optimizer='sgd', loss='mse')
+#     model.train_on_batch(x, y)
+#     after = [v.numpy() for v in model.trainable_variables]
+#     for b, a in zip(before, after):
+#         assert np.any(np.not_equal(b, a))
