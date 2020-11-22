5d79b3849af40b0766647c9c550ce95d100bbfe7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-22 15:33:24 +0100

    removed the tdv test as was not the correct one to do

--- tdv_test.py
+++ tdv_test.py
@@ -44,38 +44,3 @@ def test_tdv_change():
     for b, a in zip(before, after):
         assert np.any(np.not_equal(b, a))
         assert not np.any(np.isnan(b))
-
-@pytest.mark.parametrize('n_macro', [2, 3])
-@pytest.mark.parametrize('n_scales', [2, 3])
-def test_tdv_gradient(n_macro, n_scales):
-    # this is more of a test that tensorflow does the correct thing
-    # when applying the gradient tape since this should be taken into account
-    # but since I heard some ppl telling there might be silent bugs, I prefer
-    # checking
-    # setup the data
-    x = tf.random.normal((1, 32, 32, 1))
-
-    # define the TDV regularizer
-    model = TDV(
-        n_macro=n_macro,
-        n_scales=n_scales,
-        n_filters=8,
-    )
-
-    def compute_loss(scale):
-        return model.energy(scale*x)
-
-    scale = 1.
-
-    # compute the gradient using the implementation
-    grad_scale = tf.reduce_sum(x*model(scale*x)).numpy()
-
-    # check it numerically
-    epsilon = 1e-4
-    l_p = compute_loss(scale+epsilon).numpy()
-    l_n = compute_loss(scale-epsilon).numpy()
-    grad_scale_num = (l_p - l_n) / (2 * epsilon)
-
-    condition = np.abs(grad_scale - grad_scale_num) / grad_scale_num < 1e-2
-    print(f'grad_scale: {grad_scale} num_grad_scale {grad_scale_num} success: {condition}')
-    assert condition
