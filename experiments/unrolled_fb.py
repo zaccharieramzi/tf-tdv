d7d4a0f41cb2379e2caeefe1c96db1fad1708617
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-11-22 15:32:53 +0100

    defined an extra layer for unrolled fb in order to have all weights saved correctly

--- experiments/unrolled_fb.py
+++ experiments/unrolled_fb.py
@@ -1,8 +1,47 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer
 
 
 EPSILON = tf.constant(1e-6)
+
+class GradStep(Layer):
+    def __init__(self, reg_grad, inverse_problem='denoising', init_step_size=0.5, **kwargs):
+        super().__init__(**kwargs)
+        self.reg_grad = reg_grad
+        self.inverse_problem = inverse_problem
+        self.init_step_size = init_step_size
+        # I thought that the 2 step sizes were common
+        # but in the code it appears that they are not:
+        # https://github.com/VLOGroup/tdv/blob/master/model.py#L111
+        self.alpha = self.add_weight(
+            shape=(1,),
+            initializer=tf.keras.initializers.constant(self.init_step_size),
+            constraint=tf.keras.constraints.NonNeg(),
+            name='alpha',
+        )
+        self.lamda = self.add_weight(
+            shape=(1,),
+            initializer=tf.keras.initializers.constant(10*self.init_step_size),
+            constraint=tf.keras.constraints.NonNeg(),
+            name='lambda',
+        )
+        if self.inverse_problem == 'denoising':
+            self.measurements_operator = lambda x: x
+            self.measurements_operator_adjoint = lambda x: x
+        else:
+            raise NotImplementedError(f'{self.inverse_problem} is not implemented yet')
+
+    def grad_measures(self, image, inputs):
+        gr = self.measurements_operator_adjoint(self.measurements_operator(image) - inputs)
+        return gr
+
+    def call(self, inputs):
+        current_image, inputs = inputs
+        reg_grad_eval = (self.alpha + EPSILON) * self.reg_grad(current_image)
+        grad_eval = (self.lamda + EPSILON) * self.grad_measures(current_image, inputs)
+        new_image = current_image - grad_eval - reg_grad_eval
+        return new_image
 
 class UnrolledFB(Model):
     """The unrolled Forward-Backward model with any kind of regularizer gradient.
@@ -24,45 +63,24 @@ class UnrolledFB(Model):
         self.init_step_size = init_step_size
         if self.weight_sharing:
             self.original_reg_grad = model_class(**model_kwargs)
-        self.reg_grads = [
-            self.original_reg_grad if self.weight_sharing else model_class(**model_kwargs)
-            for _ in range(self.n_iter)
-        ]
-        # I thought that the 2 step sizes were common
-        # but in the code it appears that they are not:
-        # https://github.com/VLOGroup/tdv/blob/master/model.py#L111
-        # TODO: It might be best not to have the weights in the model as they will
-        # not be saved in the model weights ...
-        self.alpha = self.add_weight(
-            shape=(1,),
-            initializer=tf.keras.initializers.constant(self.init_step_size),
-            constraint=tf.keras.constraints.NonNeg(),
-            name='alpha',
-        )
-        self.lamda = self.add_weight(
-            shape=(1,),
-            initializer=tf.keras.initializers.constant(10*self.init_step_size),
-            constraint=tf.keras.constraints.NonNeg(),
-            name='lambda',
-        )
-        if self.inverse_problem == 'denoising':
-            self.measurements_operator = lambda x: x
-            self.measurements_operator_adjoint = lambda x: x
         else:
-            raise NotImplementedError(f'{self.inverse_problem} is not implemented yet')
-
-
-    def grad(self, image, inputs):
-        gr = self.measurements_operator_adjoint(self.measurements_operator(image) - inputs)
-        return gr
+            raise NotImplementedError('For now we only have a single reg')
+        # self.reg_grads = [
+        #     self.original_reg_grad if self.weight_sharing else model_class(**model_kwargs)
+        #     for _ in range(self.n_iter)
+        # ]
+        self.reg_grads = [self.original_reg_grad]
+        self.gradient_step = GradStep(
+            self.original_reg_grad,
+            inverse_problem=inverse_problem,
+            init_step_size=init_step_size,
+        )
 
 
     def call(self, inputs):
         current_image = inputs
-        for reg_grad in self.reg_grads:
-            reg_grad_eval = (self.alpha + EPSILON) * reg_grad(current_image)
-            grad_eval = (self.lamda + EPSILON) * self.grad(current_image, inputs)
-            new_image = current_image - grad_eval - reg_grad_eval
+        for _ in range(self.n_iter):
+            new_image = self.gradient_step([current_image, inputs])
             # NOTE: we use this decorrelation of var names to allow for
             # Nesterov implementation
             current_image = new_image
